1d3573a43876de91cba84b06f163c4bbbe15355d
ncheema <ncheema@ucsd.edu>
2015-01-30 15:06:25 -0800

    make it modular - completed

--- modular.js
+++ modular.js
@@ -1,8 +1,10 @@
 var mod = require('./lsModule');
 
 mod(process.argv[2],process.argv[3], function(err,list) {
-	
+	if (err) console.error("Error: ", err);
+	else {
 		for (var i = 0; i< list.length; i++)
-			console.log(list[i]);
+					console.log(list[i]);
 		
+	}	
 });
