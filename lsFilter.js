fce18d60c858577536330d492c238bd36480ef7c
ncheema <ncheema@ucsd.edu>
2015-01-30 15:00:19 -0800

    Fielted LS- Completed

---
+++ lsFilter.js
@@ -0,0 +1,7 @@
+var fs = require('fs');
+var path = require('path')
+result = fs.readdir(process.argv[2], function(err,list){
+	for (var i = 0; i < list.length; i++) 
+		if (path.extname(list[i]) == "."+process.argv[3])
+			console.log(list[i])
+});
